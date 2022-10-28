2eb7f914e54bedff13734af313239966ea6d8ebd
Zaidkhalid44 <113784125+Zaidkhalid44@users.noreply.github.com>
2022-10-28 20:01:36 +0530

    Update 13.c

--- Assignment8/13.c
+++ Assignment8/13.c
@@ -1,18 +1,21 @@
-#include<stdio.h>// incomplete
+#include<stdio.h>
 int main()
 {
     int i,j,count='A';
     for(i=0;i<=6;i++)
     {
         count='A';
-        for(j=0;j<=6-i;j++)
-        printf("%c",count++);
-        for(j=0;j<=2*i+1;j++)
-        printf(" ");
-        if(i==0)
-        count--;
-        while(count=='A')
-        printf("%c",count--);
+        for(j=0;j<=12;j++)
+        {
+            if(j<=6-i || j>=6+i){
+            printf("%c",count);
+            j<6?count++:count--;}
+            else{
+            printf(" ");
+            if(j==6)
+            --count;}
+
+        }
         printf("\n");
         
     }
